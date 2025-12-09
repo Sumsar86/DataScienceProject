08ae8961de57c9950f7c6a20fd551f0246507605
kaurking <kaur.kingsepp13.com>
2025-12-09 16:15:18 +0200

    i think the main thing is done now. Unfortunately we (including me (dissapointed)) didn't realise the data was not going to give any meaningful results

--- src/data/label_processing.py
+++ src/data/label_processing.py
@@ -1,5 +1,18 @@
 from sklearn.preprocessing import LabelEncoder
 
+
+class LabelProcessor:
+    def __init__(self):
+        self.order = ["Poor", "Needs Improvement", "Satisfactory", "Good", "Excellent"]
+        self.label_to_int = {label: i for i, label in enumerate(self.order)}
+        self.int_to_label = {i: label for i, label in enumerate(self.order)}
+
+    def encode_labels(self, y):
+        return y.map(self.label_to_int)
+
+    def decode_labels(self, y_int):
+        return y_int.map(self.int_to_label)
+'''
 class LabelProcessor:
     def __init__(self):
         self.le = LabelEncoder()
@@ -11,3 +24,4 @@ class LabelProcessor:
     def decode_labels(self, y):
         y_labels = self.le.inverse_transform(y)
         return y_labels
+'''
--- src/data/split.py
+++ src/data/split.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+'''
 def split_data(X, y, test_size=0.2, val_size=0.1, random_state=42):
     # Split the data into training and temporary sets
     X_train_data, X_temp_data, y_train_data, y_temp_data = train_test_split(X, y, test_size=test_size, random_state=random_state)
@@ -12,6 +13,23 @@ def split_data(X, y, test_size=0.2, val_size=0.1, random_state=42):
     X_val_data, X_test_data, y_val_data, y_test_data = train_test_split(X_temp_data, y_temp_data, test_size=new_val_size, random_state=random_state)
     
     return X_train_data, X_val_data, X_test_data, y_train_data, y_val_data, y_test_data
+'''
+def split_data(X, y, test_size=0.2, val_size=0.1, random_state=42):
+    """
+    Splits X and y into train, validation, and test sets.
+    Uses stratification to preserve class distribution.
+    """
+    
+    # split into train and temp (Val + Test)
+    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=test_size, stratify=y, random_state=random_state)
+    
+    # Compute ratio of val inside the remaining temp set
+    val_ratio = val_size / (1 - test_size)
+
+    # Split temporary set into validation and test
+    X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=val_ratio, random_state=random_state)
+    
+    return X_train, X_val, X_test, y_train, y_val, y_test
 
 # def save_splits(train_data, val_data, test_data, output_dir):
 #     train_data.to_csv(f"{output_dir}/train_data.csv", index=False)
